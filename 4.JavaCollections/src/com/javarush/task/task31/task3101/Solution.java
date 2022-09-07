d89d103f27113acf0ec94aba7fbdb7acdea062a2
Geraskin Denis <fresh7tea@gmail.com>
2022-09-07 22:52:05 +0300

    https://javarush.ru/tasks/com.javarush.task.task31.task3101 Решение от JavaRush
    Signed-off-by: Geraskin Denis <fresh7tea@gmail.com>

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /* 
 Проход по дереву файлов
@@ -14,64 +18,52 @@ public class Solution {
     public static void main(String[] args) {
         // "temp/task3101/src" "c:/fresh7lemon@ya.ru/java/vm53/IdeaProjects/JavaRushTasks/temp/task3101/result.txt"
 
-        if (args.length != 2) {
-            return;
-        }
+        String path = args[0];
+        String resultFileAbsolutePath = args[1];
+        try {
+            File resultFile = new File(resultFileAbsolutePath);
+            File dest = new File(resultFile.getParentFile() + "/allFilesContent.txt");
+            if (FileUtils.isExist(dest)) {
+                FileUtils.deleteFile(dest);
+            }
+            FileUtils.renameFile(resultFile, dest);
 
-        File src = new File(System.getProperty("user.dir") + "/" + args[0]);
-        if (!FileUtils.isExist(src)) {
-            System.out.println("The catalog '" + src.getAbsolutePath() + "' not found!");
-            return;
-        }
-
-        File resultFile = new File(args[1]);
-        if(!FileUtils.isExist(resultFile)){
-            System.out.println("The File '" + resultFile.getAbsolutePath() + "' not found!");
-            return;
-        }
-
-        File newResultFile = new File(resultFile.getParent() + "/allFilesContent.txt");
-
-        // Если существует файл с новым именем, тогда удаляем его
-        if (FileUtils.isExist(newResultFile)) {
-            FileUtils.deleteFile(newResultFile);
-        }
-
-        if (FileUtils.isExist(resultFile)) {
-            FileUtils.renameFile(resultFile, newResultFile);
-        }
-
-        try (FileOutputStream fos = new FileOutputStream(newResultFile)) {
-            readCatalogRecursion(src, fos);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void readCatalogRecursion(File Directory, FileOutputStream fos) {
-        File[] listFiles = Directory.listFiles();
-        for (File file : listFiles) {
-            if (file.isDirectory()) {
-                readCatalogRecursion(file, fos);
-            } else {
-                if (file.length() <= 50L && isTxtFile(file)) {
-                    try (FileInputStream fis = new FileInputStream(file)) {
-                        byte[] bytes = new byte[16];
-                        while (fis.available() > 0) {
-                            int n = fis.read(bytes);
-                            fos.write(bytes, 0, n);
-                        }
-                        fos.write(System.lineSeparator().getBytes());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+            Map<String, byte[]> fileTree = getFileTree(path);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(dest)) {
+                for (byte[] bytes : fileTree.values()) {
+                    fileOutputStream.write(bytes);
+                    fileOutputStream.write("\n".getBytes());
                 }
             }
+        } catch (IOException ignored) {
         }
-
     }
 
-    private static boolean isTxtFile(File file) {
-        return file.getName().matches(".+.txt$");
+    public static Map<String, byte[]> getFileTree(String root) throws IOException {
+        Map<String, byte[]> result = new TreeMap<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private Map<String, byte[]> result;
+
+        public GetFiles(Map<String, byte[]> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            File file = path.toFile();
+            if (file.isFile()) {
+                if (file.length() <= 50) {
+                    result.put(path.getFileName().toString(), Files.readAllBytes(path));
+                }
+            }
+            return super.visitFile(path, basicFileAttributes);
+        }
     }
 }
